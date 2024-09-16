21a41a77fe03b43a1aece9abf3db28040e284259
Adrian <119546624+GuillenA7@users.noreply.github.com>
2024-09-16 15:44:30 -0600

    Add files via upload

---
+++ main/java/com/example/prctica03_activityconarreglosdeobjetos/Motocicleta.kt
@@ -0,0 +1,8 @@
+package com.example.prctica03_activityconarreglosdeobjetos
+
+data class Motocicleta(
+    var numeroSerie: String = "",
+    var marca: String = "",
+    var modelo: String = "",
+    var cilindrada: Int = 0
+)
